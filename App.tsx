c003c6c06fbee75b6c9acdbe7f87afbc2a0b5fd2
AngryMailbox <mans@klintan.net>
2022-12-16 18:41:40 +0100

    updated paths

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import Navbar from "../components/appbar";
-import BottomNav from "../components/bottomNav";
+import Navbar from "./components/appbar";
+import BottomNav from "./components/bottomNav";
 import { Provider as PaperProvider } from "react-native-paper";
 
 import { SafeAreaProvider } from "react-native-safe-area-context";
